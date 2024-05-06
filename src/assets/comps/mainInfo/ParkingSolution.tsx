5f294df54cd015fdddd7ea3fb6934ef53e77f5dc
Nitai <nitaidaud@gmail.com>
2024-05-06 21:29:29 +0300

    performance testing

--- src/assets/comps/mainInfo/ParkingSolution.tsx
+++ src/assets/comps/mainInfo/ParkingSolution.tsx
@@ -5,7 +5,7 @@ export default function ParkingSolution() {
     <div className="grid gap-9">
       <div id="start" className="w-3/5 m-auto">
         <img
-        loading="lazy"
+          loading="lazy"
           src="..\..\Images\parkingSolutionImg.png"
           alt="introduction image"
         />
