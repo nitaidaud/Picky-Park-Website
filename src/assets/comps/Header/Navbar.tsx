dbf01ee91e1f8125a2054138b644f0c6a0f72463
Nitai <nitaidaud@gmail.com>
2024-05-07 16:37:16 +0300

    bg color fixed

--- src/assets/comps/Header/Navbar.tsx
+++ src/assets/comps/Header/Navbar.tsx
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <nav className="bg-transparent backdrop-blur-xl fixed w-full z-20 top-0 start-0">
@@ -51,35 +53,35 @@ export default function Navbar() {
           id="navbar-sticky"
         >
           <ul className="nav-links flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-            <li>
+            <li className="border-2 p-2 rounded-2xl border-transparent hover:border-white hover:bg-white duration-300">
               <a
                 href="#"
-                className="block py-2 px-3 text-white md:p-0"
+                className="block py-2 px-3 text-white md:p-0 duration-300"
                 aria-current="page"
               >
                 Home
               </a>
             </li>
-            <li>
+            <li className="border-2 p-2 rounded-2xl border-transparent hover:border-white hover:bg-white duration-300">
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-white md:p-0 duration-300"
               >
                 About
               </a>
             </li>
-            <li>
+            <li className="border-2 p-2 rounded-2xl border-transparent hover:border-white hover:bg-white duration-300">
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-white md:p-0 duration-300"
               >
                 Services
               </a>
             </li>
-            <li>
+            <li className="border-2 p-2 rounded-2xl border-transparent hover:border-white hover:bg-white duration-300">
               <a
                 href="#"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                className="block py-2 px-3 text-white md:p-0 duration-300"
               >
                 Contact
               </a>
