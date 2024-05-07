f72b2b668ae770c91592332c560103df4346400e
Nitai <nitaidaud@gmail.com>
2024-05-07 15:59:13 +0300

    background color added

--- src/assets/comps/Main.tsx
+++ src/assets/comps/Main.tsx
@@ -1,9 +1,11 @@
 import PickyLogo from "./introduction/PickyLogo";
 import IntroductionInfo from "./introduction/IntroductionInfo";
+import Navbar from "./Header/Navbar";
 
 export default function Main() {
   return (
     <>
+      <Navbar />
       <PickyLogo />
       <IntroductionInfo />
     </>
