ce643b132d41c6daf43b3bb7a9843353e356a89a
Nitai <nitaidaud@gmail.com>
2024-05-08 15:18:57 +0300

    navbar logo improved

--- src/assets/comps/Header/Navbar.tsx
+++ src/assets/comps/Header/Navbar.tsx
@@ -10,6 +10,7 @@ export default function Navbar() {
             loading="lazy"
             src="..\Images\pickyLogoImgFixed.png"
             alt="Picky Logo"
+            className="w-24 2xl:w-32"
             width={130}
             height={130}
           />
