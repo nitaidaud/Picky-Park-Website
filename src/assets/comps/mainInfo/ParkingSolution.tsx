982fe6fc3caf9e39210aa64c6d3e7e6878f2648c
Nitai <nitaidaud@gmail.com>
2024-05-23 16:32:47 +0300

    nav bar fixed

--- src/assets/comps/mainInfo/ParkingSolution.tsx
+++ src/assets/comps/mainInfo/ParkingSolution.tsx
@@ -2,7 +2,7 @@
 
 export default function ParkingSolution() {
   return (
-    <div className="w-full m-auto h-full min-h-screen pb-16">
+    <div className="w-full m-auto h-full min-h-screen pb-16 flex justify-center">
       <div className="grid gap-9 h-full mx-auto w-2/3">
         <div className="w-2/6 m-auto">
           <img
