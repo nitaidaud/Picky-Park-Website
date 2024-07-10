91c866ad574d0218ef30549996db445fc2ec9687
Nitai <nitaidaud@gmail.com>
2024-07-10 14:41:37 +0300

    "Refactored Introduction and ParkingSolution components: updated layout, added font awesome icons, and rearranged content."

--- src/assets/comps/mainInfo/ParkingSolution.tsx
+++ src/assets/comps/mainInfo/ParkingSolution.tsx
@@ -1,10 +1,120 @@
-// import { LazyLoadImage } from "react-lazy-load-image-component";
+import {
+  faCompass,
+  faMobileScreenButton,
+  faParking,
+  faPeopleArrows,
+  faStreetView,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ParkingSolution() {
   return (
-    <div className="w-full m-auto h-full  md:min-h-screen pb-16 flex justify-center animate-block">
-      <div className="grid gap-9 h-full mx-auto w-full 2xl:w-2/3">
-        <div className="w-1/2 sm:w-1/3 lg:w-2/6 m-auto">
+    <div className="w-full m-auto h-full md:h-full p-32 flex items-center justify-center animate-block tracking-normal">
+      <div className="grid grid-cols-2 gap-2 h-full mx-auto w-full 2xl:w-2/3 px-5">
+        <div className="flex justify-between items-center gap-3 w-full text-balance h-full text-center bg-indigo-950 rounded-3xl">
+          <div className="flex flex-col justify-center items-start w-full px-4 2xl:w-4/5 gap-10 lg:gap-16 xl:gap-20 h-full mx-auto ">
+            <div className="flex justify-start items-center gap-4 w-full">
+              <div className="bg-indigo-900 px-2 py-3 min-w-max w-1/5 rounded-xl justify-center items-center">
+                <FontAwesomeIcon icon={faParking} size="2x" />
+              </div>
+
+              <div className="flex flex-col text-start">
+                <div>
+                  <h2>Easy Way To Park</h2>
+                </div>
+                <div className="text-xs opacity-80">
+                  <p className="capitalize">
+                    Picky takes the stress out of parking
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-start items-center gap-4 w-full">
+              <div className="bg-indigo-900 px-2 py-3 min-w-max w-1/5 rounded-xl justify-center items-center">
+                <FontAwesomeIcon icon={faStreetView} size="2x" />
+              </div>
+
+              <div className="flex flex-col text-start">
+                <div>
+                  <h2>Find Parking Around You</h2>
+                </div>
+                <div className="text-xs opacity-80">
+                  <p className="capitalize">
+                    Picky providing real-time information on available parking
+                    spaces
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-start items-center gap-4 w-full">
+              <div className="bg-indigo-900 px-2 py-3 min-w-max w-1/5 rounded-xl justify-center items-center">
+                <FontAwesomeIcon icon={faPeopleArrows} size="2x" />
+              </div>
+
+              <div className="flex flex-col text-start">
+                <div>
+                  <h2>Sharing Parkings</h2>
+                </div>
+                <div className="text-xs opacity-80">
+                  <p className="capitalize">
+                    Choose Between A Lot Of available Parkings
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-start items-center gap-4 w-full">
+              <div className="bg-indigo-900 px-2 py-3 min-w-max w-1/5 rounded-xl justify-center items-center">
+                <FontAwesomeIcon icon={faCompass} size="2x" />
+              </div>
+
+              <div className="flex flex-col text-start">
+                <div>
+                  <h2>Explore Your Next Park</h2>
+                </div>
+                <div className="text-xs opacity-80">
+                  <p className="capitalize">
+                    guiding you to the closest and most convenient spots
+                    effortlessly
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="flex justify-start items-center gap-4 w-full">
+              <div className="bg-indigo-900 px-2 py-3 min-w-max w-1/5 rounded-xl justify-center items-center">
+                <FontAwesomeIcon icon={faMobileScreenButton} size="2x" />
+              </div>
+
+              <div className="flex flex-col text-start">
+                <div>
+                  <h2>User Friendly Interface</h2>
+                </div>
+                <div className="text-xs opacity-80">
+                  <p className="capitalize">
+                    The app's user-friendly interface ensures a smooth
+                    experience
+                  </p>
+                </div>
+              </div>
+            </div>
+
+            {/* <h2 className="intro-title parking-title uppercase tracking-tight text-5xl font-black bg-clip-text animate-block">
+              Effortless Parking Solutions
+            </h2>
+
+            <p className="paragraph-info text-white  text-xl font-extralight bg-clip-text animate-block">
+              Picky takes the stress out of parking by providing users with
+              real-time information on available parking spaces. The app's
+              user-friendly interface ensures a smooth experience, guiding you
+              to the closest and most convenient spots effortlessly.
+            </p> */}
+          </div>
+        </div>
+
+        <div className="bg-indigo-950 rounded-3xl h-full w-full mx-auto py-20">
           <img
             width={"100%"}
             height={"100%"}
@@ -12,32 +122,6 @@ export default function ParkingSolution() {
             src="..\..\Images\parkingSolutionImg.png"
             alt="introduction image"
           />
-        </div>
-        <div className="flex justify-between items-center gap-3 w-full text-balance h-full text-center">
-          <div className="grid w-full px-4 2xl:w-3/5 gap-9 h-full m-auto ">
-            <h2 className="intro-title parking-title uppercase tracking-tight text-5xl font-black bg-clip-text animate-block">
-              Effortless Parking Solutions
-            </h2>
-            <p className="paragraph-info text-white  text-xl font-extralight bg-clip-text animate-block">
-              Picky takes the stress out of parking by providing users with
-              real-time information on available parking spaces. The app's
-              user-friendly interface ensures a smooth experience, guiding you
-              to the closest and most convenient spots effortlessly.
-            </p>
-          </div>
-
-          <div className="grid w-2/5 gap-9 h-full">
-            <h2 className="intro-title parking-title uppercase tracking-tight text-5xl font-black bg-clip-text">
-              Lorem, ipsum dolor.
-            </h2>
-            <p className="paragraph-info text-white  text-xl font-extralight bg-clip-text">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea qui
-              amet quas molestias ipsa iste eius doloribus labore, eos
-              blanditiis esse sequi. Cupiditate nemo, dolorem expedita nobis
-              ducimus odio fugiat!
-              sad sdad d saasdsadsad sad as 
-            </p>
-          </div>
         </div>
       </div>
     </div>
