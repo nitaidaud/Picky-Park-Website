d5dc4f4868b796fee56b45ee0b170fd5609637a2
Nitai <nitaidaud@gmail.com>
2024-07-21 15:27:36 +0300

    "Moved owl carousel initialization inside conditional check and added dependency to useEffect"

--- src/App.tsx
+++ src/App.tsx
@@ -4,39 +4,39 @@ import { BrowserRouter as Router } from "react-router-dom";
 import AppRouts from "./assets/comps/AppRouts";
 
 function App() {
+  const owl = $(".owl-carousel");
 
   useEffect(() => {
-    
-    const owl = $(".owl-carousel");
-    owl.owlCarousel({
-      responsive: {
-        0: {
-          items: 1,
-        },
+    if (owl)
+      owl.owlCarousel({
+        responsive: {
+          0: {
+            items: 1,
+          },
 
-        480: {
-          items: 1,
-        },
+          480: {
+            items: 1,
+          },
 
-        768: {
-          items: 2,
-        },
+          768: {
+            items: 2,
+          },
 
-        938: {
-          items: 3,
+          938: {
+            items: 3,
+          },
         },
-      },
-      loop: true,
-      lazyLoad: true,
-      autoplay: true,
-      // autoplayTimeout: 1000,
-      autoplaySpeed: 1000,
-      slideTransition: "linear",
-      fluidSpeed: true,
-      rewind: false,
-      autoplayHoverPause: true,
-    });
-  }, []);
+        loop: true,
+        lazyLoad: true,
+        autoplay: true,
+        // autoplayTimeout: 1000,
+        autoplaySpeed: 1000,
+        slideTransition: "linear",
+        fluidSpeed: true,
+        rewind: false,
+        autoplayHoverPause: true,
+      });
+  }, [owl]);
 
   return (
     <>
