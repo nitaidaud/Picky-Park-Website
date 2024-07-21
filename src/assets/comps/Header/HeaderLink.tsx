6d89231094607e20a64b64a6999cbe23fd245424
Nitai <nitaidaud@gmail.com>
2024-07-21 11:19:57 +0300

    "Updated HeaderLink and Navbar components with changes to CSS classes and added onClick event handler to Navbar link"

--- src/assets/comps/Header/HeaderLink.tsx
+++ src/assets/comps/Header/HeaderLink.tsx
@@ -13,7 +13,7 @@ export default function HeaderLink(props: {
       href={href}
       aria-current="page"
     >
-      <div className="block py-2 px-3 text-white md:p-0 duration-300">
+      <div className="block py-2 px-3 text-white md:p-0 duration-300 text-5xl md:text-base">
         {content}
       </div>
     </a>
