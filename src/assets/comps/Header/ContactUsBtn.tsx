bab1e2ba9deb15f0d8586e07c067fe0e2af8a68d
Nitai <nitaidaud@gmail.com>
2024-07-21 18:06:40 +0300

    "Updated CSS styles and HTML structure for App, Footer, Header, and Navbar components"

--- src/assets/comps/Header/ContactUsBtn.tsx
+++ src/assets/comps/Header/ContactUsBtn.tsx
@@ -3,7 +3,7 @@ export default function ContactUsBtn() {
   return (
     <a
       href="#contact-info"
-      className="contact-btn uppercase text-white font-medium rounded-3xl hover:ease-in-out hover:border-transparent text-center duration-300 "
+      className="contact-btn uppercase text-white font-medium rounded-3xl hover:ease-in-out hover:border-transparent text-center duration-300"
     >
       Contact Us
     </a>
