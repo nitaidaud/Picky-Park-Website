ef472f5ca17fe6c2ab0650113ebdc12b41f242de
Nitai <nitaidaud@gmail.com>
2024-08-07 12:04:34 +0300

    Remove ParkingSolution import from Introduction.tsx

--- src/assets/comps/Introduction.tsx
+++ src/assets/comps/Introduction.tsx
@@ -1,4 +1,4 @@
-import ParkingSolution from "./mainInfo/ParkingSolution";
+
 import MeetPicky from "./mainInfo/MeetPicky";
 import FindingPark from "./mainInfo/FindingPark";
 import Pricing from "./mainInfo/Pricing";
