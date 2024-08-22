1ae3e75e009c82121f0393254a38dd607eda5103
Nitai <nitaidaud@gmail.com>
2024-08-22 10:48:18 +0300

    "Updated App.css and Pricing.tsx: changed gradient colors, rearranged and refactored pricing item layout and styles."

--- src/assets/comps/mainInfo/Pricing.tsx
+++ src/assets/comps/mainInfo/Pricing.tsx
@@ -39,52 +39,50 @@ export default function Pricing() {
           </h2>
         </div>
         <div className="pricing-container lg:flex justify-center items-start h-fit w-3/4 2xl:w-2/3 mx-auto lg:gap-5 2xl:gap-10">
-          {pricingCards.map(
-            ({ title, price, isRecommended, coins }) => {
-              return (
-                <div
-                  key={title + price}
-                  className="pricing-item grid lg:block p-5 my-20 lg:my-0 w-full rounded-xl shadow-xl bg-white bg-opacity-70 min-h-[350px] md:min-h-[650px] lg:min-h-[350px] 2xl:min-h-[450px] h-full lg:h-4/5 hover:scale-105 duration-300 relative"
-                >
-                  {isRecommended == true && (
-                    <div className="flex absolute top-0 justify-center items-center w-full left-0 mx-auto">
-                      <h2 className="text-center w-fit px-3 py-2 bg-white bg-opacity-40 border-[2px] rounded-xl absolute -top-3 text-indigo-700 text-3xl lg:text-base">
-                        {t("pricing.sec.recommended")}
-                      </h2>
-                    </div>
-                  )}
-                  <div
-                    className={`w-full lg:h-2/6 ${
-                      title == t("pricing.first.title")
-                        ? "bg-indigo-600"
-                        : title == t("pricing.sec.title")
-                        ? "bg-red-400"
-                        : "bg-amber-300"
-                    } rounded-lg text-center flex items-center justify-center`}
-                  >
-                    <h2 className="text-black uppercase font-semibold text-4xl lg:text-xl">
-                      {title}
+          {pricingCards.map(({ title, price, isRecommended, coins }) => {
+            return (
+              <div
+                key={title + price}
+                className="pricing-item grid lg:block p-5 my-20 lg:my-0 w-full rounded-xl shadow-xl bg-white bg-opacity-70 min-h-[350px] md:min-h-[650px] lg:min-h-[350px] 2xl:min-h-[450px] h-full lg:h-4/5 hover:scale-105 duration-300 relative"
+              >
+                {isRecommended == true && (
+                  <div className="flex absolute top-0 justify-center items-center w-full left-0 mx-auto">
+                    <h2 className="text-center w-fit px-3 py-2 bg-white bg-opacity-40 border-[2px] rounded-xl absolute -top-3 text-indigo-700 text-3xl lg:text-base">
+                      {t("pricing.sec.recommended")}
                     </h2>
                   </div>
-                  <div className="flex flex-col justify-start items-center w-full lg:h-3/4">
-                    <div className="flex items-center justify-center w-full h-3/4 picky-coins">
-                      <h2 className="text-black font-bold text-7xl lg:text-3xl xl:text-4xl">
-                        {coins}{" "}
-                        <span className="block text-black font-bold text-xl">
-                          {t("pricing.pickyCoins")}
-                        </span>
-                      </h2>
-                    </div>
-                    <div className="flex items-center justify-center w-full h-1/4">
-                      <h2 className="text-black font-bold w-full 2xl:w-4/5 text-3xl h-full">
-                        {price}
-                      </h2>
-                    </div>
+                )}
+                <div
+                  className={`w-full lg:h-2/6 ${
+                    title == t("pricing.first.title")
+                      ? "bg-indigo-600"
+                      : title == t("pricing.sec.title")
+                      ? "bg-red-400"
+                      : "bg-amber-300"
+                  } rounded-lg text-center flex items-center justify-center`}
+                >
+                  <h2 className="text-black uppercase font-semibold text-4xl lg:text-xl">
+                    {title}
+                  </h2>
+                </div>
+                <div className="flex flex-col justify-center items-center w-full lg:h-3/4">
+                  <div className="flex items-center justify-center w-full h-3/4 picky-coins">
+                    <h2 className="text-black font-bold w-full 2xl:w-4/5 text-3xl h-fit">
+                      {price}
+                    </h2>
+                  </div>
+                  <div className="flex items-center justify-center w-full h-2/3">
+                    <h2 className="text-amber-500 font-bold text-7xl lg:text-3xl xl:text-4xl">
+                      {coins}{" "}
+                      <span className="block text-amber-500 font-bold text-xl">
+                        {t("pricing.pickyCoins")}
+                      </span>
+                    </h2>
                   </div>
                 </div>
-              );
-            },
-          )}
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
