948385cdc394452f6657c23d5501d9d326bee42b
Nitai <nitaidaud@gmail.com>
2024-08-23 14:08:25 +0300

    "Remove unnecessary 'React' import in Introduction.tsx and FirstSlide.tsx"

--- src/assets/comps/introduction/FirstSlide.tsx
+++ src/assets/comps/introduction/FirstSlide.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import { faApple } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useTranslation } from "react-i18next";
