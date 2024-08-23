761a9b6817d4ca0a52cf8f507e378df3e13aedb1
Nitai <nitaidaud@gmail.com>
2024-08-23 14:14:12 +0300

    "Refactored Footer/DownloadBtn and Introduction components, removed unused code and updated styles"

--- src/assets/comps/Footer/DownloadBtn.tsx
+++ src/assets/comps/Footer/DownloadBtn.tsx
@@ -2,7 +2,7 @@
 
 export default function DownloadBtn() {
   return (
-    <div className="flex w-full justify-start h-full items-end gap-3 mb-15 lg:mb-0">
+    <div className="flex w-full justify-center lg:justify-start h-full items-end gap-3 mb-15 lg:mb-0">
         <a href="https://apps.apple.com/il/app/picky-park/id6476976854" target="_blank">
             <img src="..\Images\AppStoreBtn.png" alt="App store button" width={120} />
         </a>
